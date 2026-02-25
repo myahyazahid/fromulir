1b2228877fcd6850b8b022c210ffd369f617a211
Muhammad Yahya Zahid <myahyazahid11@gmail.com>
2026-02-25 12:46:18 +0700

    complete development

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function validasiNama() {
     let nama = document.getElementById("nama").value;
     let alamat = document.getElementById("alamat").value;
     let tempat = document.getElementById("tempat").value;
-    let pola = /^[a-zA-Z']+$/;
+    let pola = /^[a-zA-Z' ]+$/;
     
 
     if(!pola.test(nama)) {
-        alert("Nama Harus Berisi huruf dan tanda petik satu (')");
+        alert("Nama Harus Berisi huruf ");
     }
     else if(!pola.test(alamat)) {
-        alert("alamat Harus Berisi huruf dan tanda petik satu (')");
+        alert("alamat Harus Berisi huruf ");
     }
     else if(!pola.test(tempat)) {
-        alert("tempat lahir Harus Berisi huruf dan tanda petik satu (')");
+        alert("tempat lahir Harus Berisi huruf ");
     }
 
     else
